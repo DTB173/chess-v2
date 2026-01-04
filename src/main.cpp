b55c830c294071e5d47deb77cba0b5c47de300a9
DTB173 <jacek92010c@gmail.com>
2026-01-04 19:11:06 +0100

    Feat: Implemented perf tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 ﻿import CLI;
+import Perft;
 #include <iostream>
 
 int main() {
-    CLI::game_loop();
+    //CLI::game_loop();
+	for (int depth = 1; depth <= 6; ++depth) {
+		std::cout << "=== PERFT Depth " << depth << " ===\n";
+		Perft::measure_perft(depth, true);
+		std::cout << "=====================\n";
+	}
+		
     return 0;
 }
